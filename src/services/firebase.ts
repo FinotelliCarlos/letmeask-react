ec1c20837058a2f107e898e7f19ec0bbb03f17e1
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2021-06-21 13:35:41 -0300

    config env.local

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import "firebase/auth";
 import "firebase/database";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDVBmT0yjq6Dw_1EVYqJxi4KY1f4oUnfmQ",
-  authDomain: "letmeask-react-f23ee.firebaseapp.com",
-  databaseURL: "https://letmeask-react-f23ee-default-rtdb.firebaseio.com",
-  projectId: "letmeask-react-f23ee",
-  storageBucket: "letmeask-react-f23ee.appspot.com",
-  messagingSenderId: "913280626992",
-  appId: "1:913280626992:web:1d90d5c504873a3ef198f5",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);

5ec2d9a2567c21701a6c450ebb907b62fd40ceaa
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2021-06-22 14:33:05 -0300

    init contextapi (AuthContextProvider)

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,52 @@ import { Home } from "./pages/Home";
 import "../src/styles/global.scss";
 import { NewRoom } from "./pages/NewRoom";
 
+import { createContext } from "react";
+
 import { BrowserRouter, Route } from "react-router-dom";
+import { useState } from "react";
+import { firebase, auth } from "./services/firebase";
+
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
+
+type AuthContextType = {
+  user: User | undefined;
+  signInWithGoogle: () => void;
+};
+
+export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
+  const [user, setUser] = useState<User>();
+
+  function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider).then((result) => {
+      if (result.user) {
+        const { displayName, photoURL, uid } = result.user;
+        if (!displayName || !photoURL) {
+          throw new Error("Missing information from Google Account.");
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL,
+        });
+      }
+    });
+  }
+
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/new" component={NewRoom} />
+      <AuthContext.Provider value={{ user, signInWithGoogle }}>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContext.Provider>
     </BrowserRouter>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { useHistory } from "react-router-dom";
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
-
+import { auth, firebase } from "../services/firebase";
 import { Button } from "../components/Button";
 
 import "../styles/auth.scss";
@@ -10,8 +10,13 @@ import "../styles/auth.scss";
 export function Home() {
   const history = useHistory();
 
-  function navigateToNewRoom() {
-    history.push("/rooms/new");
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider).then((result) => {
+      console.log(result);
+
+      history.push("/rooms/new");
+    });
   }
 
   return (
@@ -24,7 +29,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button onClick={navigateToNewRoom} className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo Google" />
             Crie sua sala com o Google
           </button>

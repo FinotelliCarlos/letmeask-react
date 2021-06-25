563e194149c6e0784596a2a8bf630609c6cd6c8c
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2021-06-25 00:11:02 -0300

    estrutura pagina room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,3 +1,33 @@
+import LogoImg from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
+import "../styles/room.scss";
+
 export function Room() {
-  return <h1>Sala</h1>;
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={LogoImg} alt="Letmeask" />
+          <div>codigo</div>
+        </div>
+      </header>
+
+      <main className="content">
+        <div className="room-title">
+          <h1>SalaReact</h1>
+          <span>4 asks</span>
+        </div>
+
+        <form>
+          <textarea placeholder="Diga oque que gostaria de perguntar:" />
+          <div className="form-footer">
+            <span>
+              Parta enviar sua pergunta, <button>faça seu login.</button>
+              <Button type="submit">Enviar pergunta</Button>
+            </span>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 }

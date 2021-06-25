0c7d326acbf6a3e363c399297bb6f7cdc2e28a9c
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2021-06-25 13:19:41 -0300

    Update Room.tsx

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -2,14 +2,21 @@ import LogoImg from "../assets/images/logo.svg";
 import { Button } from "../components/Button";
 import "../styles/room.scss";
 import { RoomCode } from "../components/RoomCode";
+import { useParams } from "react-router-dom";
+
+type RoomParams = {
+  id: string;
+};
 
 export function Room() {
+  const params = useParams<RoomParams>();
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={LogoImg} alt="Letmeask" />
-          <RoomCode code="123321123312123312" />
+          <RoomCode code={params.id} />
         </div>
       </header>
 

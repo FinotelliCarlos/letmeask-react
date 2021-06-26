88f3f1b06adab2c1c3c999f979095f65828e4381
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2021-06-26 00:06:06 -0300

    create function admin remove question

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -5,10 +5,11 @@ import { RoomCode } from "../components/RoomCode";
 import { useParams } from "react-router-dom";
 // import { FormEvent, useState } from "react";
 // import { useAuth } from "../hooks/useAuth";
-// import { database } from "../services/firebase";
+import { database } from "../services/firebase";
 // import { useEffect } from "react";
 import { Question } from "../components/Question";
 import { useRoom } from "../hooks/useRoom";
+import deleteImg from "../assets/images/delete.svg";
 
 type RoomParams = {
   id: string;
@@ -19,6 +20,12 @@ export function AdminRoom() {
   const params = useParams<RoomParams>();
   const roomId = params.id;
   const { questions, title } = useRoom(roomId);
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm("Tem certeza que você deseja excluir está pergunta?")) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
 
   return (
     <div id="page-room">
@@ -50,7 +57,14 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
-              />
+              >
+                <button
+                  type="button"
+                  onClick={() => handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="Remover pergunta" />
+                </button>
+              </Question>
             );
           })}
         </div>
